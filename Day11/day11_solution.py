b18ac727c3abadf4a1d921c649f97423f42207c0
Travis Thompson <tthompson691@gmail.com>
2021-12-12 21:51:58 -0600

    day 11 progress

--- Day11/day11_solution.py
+++ Day11/day11_solution.py
@@ -3,79 +3,81 @@ import numpy as np
 
 
 def flash(octos: np.array, i, j):
-    octos[i, j] = 0
-    # up left
-    if i > 0 and j > 0:
-        if octos[i - 1, j - 1] in range(1, 9):
-            octos[i - 1, j - 1] += 1
-        else:
-            return flash(octos, i - 1, j - 1)
+    if octos[i, j] == 9:
+        octos[i, j] = 0
+        # up left
+        if i > 0 and j > 0:
+            if octos[i - 1, j - 1] in range(1, 9):
+                octos[i - 1, j - 1] += 1
+            elif octos[i - 1, j - 1] == 9:
+                return flash(octos, i - 1, j - 1)
 
-    # directly up
-    if i > 0:
-        if octos[i - 1, j] in range(1, 9):
-            octos[i - 1, j] += 1
-        else:
-            return flash(octos, i - 1, j)
+        # directly up
+        if i > 0:
+            if octos[i - 1, j] in range(1, 9):
+                octos[i - 1, j] += 1
+            elif octos[i - 1, j] == 9:
+                return flash(octos, i - 1, j)
 
-    # up right
-    if i > 0 and j < octos.shape[1] - 1:
-        if octos[i - 1, j + 1] in range(1, 9):
-            octos[i - 1, j + 1] += 1
-        else:
-            return flash(octos, i - 1, j + 1)
+        # up right
+        if i > 0 and j < octos.shape[1] - 1:
+            if octos[i - 1, j + 1] in range(1, 9):
+                octos[i - 1, j + 1] += 1
+            elif octos[i - 1, j + 1] == 9:
+                return flash(octos, i - 1, j + 1)
 
-    # left
-    if j > 0:
-        if octos[i, j - 1] in range(1, 9):
-            octos[i, j - 1] += 1
-        else:
-            return flash(octos, i, j - 1)
+        # left
+        if j > 0:
+            if octos[i, j - 1] in range(1, 9):
+                octos[i, j - 1] += 1
+            elif octos[i, j - 1] == 9:
+                return flash(octos, i, j - 1)
 
-    # right
-    if j < octos.shape[1] - 1:
-        if octos[i, j + 1] != 9:
-            octos[i, j + 1] += 1
-        else:
-            return flash(octos, i, j + 1)
+        # right
+        if j < octos.shape[1] - 1:
+            if octos[i, j + 1] in range(1, 9):
+                octos[i, j + 1] += 1
+            elif octos[i, j + 1] == 9:
+                return flash(octos, i, j + 1)
 
-    # down left
-    if i < octos.shape[0] - 1 and j > 0:
-        if octos[i + 1, j - 1] != 9:
-            octos[i + 1, j - 1] += 1
-        else:
-            return flash(octos, i + 1, j - 1)
+        # down left
+        if i < octos.shape[0] - 1 and j > 0:
+            if octos[i + 1, j - 1] in range(1, 9):
+                octos[i + 1, j - 1] += 1
+            elif octos[i + 1, j - 1] == 9:
+                return flash(octos, i + 1, j - 1)
 
-    # down
-    if i < octos.shape[0] - 1:
-        if octos[i + 1, j] != 9:
-            octos[i + 1, j] += 1
-        else:
-            return flash(octos, i + 1, j)
+        # down
+        if i < octos.shape[0] - 1:
+            if octos[i + 1, j] in range(1, 9):
+                octos[i + 1, j] += 1
+            elif octos[i + 1, j] == 9:
+                return flash(octos, i + 1, j)
 
-    # down right
-    if i < octos.shape[0] - 1 and j < octos.shape[1] - 1:
-        if octos[i + 1, j + 1] != 9:
-            octos[i + 1, j + 1] += 1
-        else:
-            return flash(octos, i + 1, j + 1)
+        # down right
+        if i < octos.shape[0] - 1 and j < octos.shape[1] - 1:
+            if octos[i + 1, j + 1] in range(1, 9):
+                octos[i + 1, j + 1] += 1
+            elif octos[i + 1, j + 1] == 9:
+                return flash(octos, i + 1, j + 1)
+    else:
+        octos[i, j] += 1
 
     return octos
 
 
 def octo_step(octos):
-    octos = octos + 1
     for i, x in enumerate(octos):
         for j, y in enumerate(x):
-            if y == 9:
+            while not any(octos == 9):
                 octos = flash(octos, i, j)
-            print("D")
+                print("D")
 
     return octos
 
 
 if __name__ == "__main__":
-    octopi = np.array([list(map(int, i)) for i in read_input("day11_sample_input.txt")])
+    octopi = np.array([list(map(int, i)) for i in read_input("day11_small_input.txt")])
 
     for i in range(0, 100):
         octopi = octo_step(octopi)
