dbf6b07e3e84f4557e185ddee045626140843d95
tthompson2 <tthompson2@eprod.com>
2021-12-16 12:55:13 -0600

    day14 solutions done

--- Day13/day13_solution.py
+++ Day13/day13_solution.py
@@ -3,13 +3,11 @@ import numpy as np
 
 
 def fold(paper, axis, val):
-    # drop dots I don't care about (either to the right of the vert axis or above the horizontal axis)
     if axis == "x":
         dots = [(dot[0], dot[1]) for dot in np.argwhere(paper) if dot[1] > val]
         for dot in dots:
             diff = abs(val - dot[1])
             paper[dot[0], (dot[1] - diff * 2)] = 1
-
         del_range = list(range(val, paper.shape[1]))
         paper = np.delete(paper, del_range, 1)
     elif axis == "y":
@@ -17,7 +15,6 @@ def fold(paper, axis, val):
         for dot in dots:
             diff = abs(val - dot[0])
             paper[(dot[0] - diff * 2), dot[1]] = 1
-
         del_range = list(range(val, paper.shape[0]))
         paper = np.delete(paper, del_range, 0)
 
@@ -26,13 +23,11 @@ def fold(paper, axis, val):
 
 if __name__ == "__main__":
     inp = read_input("day13_input.txt")
-
     coords = [
         (int(i.split(",")[1]), int(i.split(",")[0]))
         for i in inp
         if "fold" not in i and i != ""
     ]
-
     instructions = [
         (
             i.split("fold along ")[1].split("=")[0],
@@ -52,7 +47,7 @@ if __name__ == "__main__":
 
     # part 1
     paper = fold(paper, instructions[0][0], instructions[0][1])
-
+    print(f"part 1: {np.count_nonzero(paper)}")
     # part 2
     paper2 = np.zeros([max_x + 1, max_y + 1], dtype=int)
 
@@ -60,4 +55,3 @@ if __name__ == "__main__":
         paper2[coord[0], coord[1]] = 1
     for instruction in instructions:
         paper2 = fold(paper2, instruction[0], instruction[1])
-    print(f"part 1: {np.count_nonzero(paper)}")
